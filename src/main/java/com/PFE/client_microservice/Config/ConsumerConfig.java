1375dbdc8441866e51d2b3e80ac529c4ef8cc095
IAMDEH <ee_izri@esi.dz>
2021-04-03 22:18:12 +0100

    Removed unused imports

--- src/main/java/com/PFE/client_microservice/Config/ConsumerConfig.java
+++ src/main/java/com/PFE/client_microservice/Config/ConsumerConfig.java
@@ -1,15 +1,10 @@
 package com.PFE.client_microservice.Config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
